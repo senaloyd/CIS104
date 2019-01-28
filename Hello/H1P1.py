77b48b2dbe18caa1c3159fb71bc3860c94e78819
senaloyd <senaloyd@gmail.com>
2019-01-27 21:08:52 -0800

    With Extra Credit

--- Hello/H1P1.py
+++ Hello/H1P1.py
@@ -4,3 +4,8 @@ age = int(input("What is your age? "))
 confidence = int(input("What is your confidence in programmers between 0-100%? "))
 dog_age = int(age * 7)
 print("Hello", first_name, last_name,", nice to meet you! You might be", age, "in human years, but in dog years you are", dog_age,".")
+confidence_level = (confidence/100.0)
+if confidence_level < 0.5:
+    print("I agree, programmers can't be trusted!")
+else:
+    print("Writing good code takes hard work!")

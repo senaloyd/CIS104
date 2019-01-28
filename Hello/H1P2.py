9710982824de223cc1fcf303fa0a6b0d062e17f9
senaloyd <senaloyd@gmail.com>
2019-01-27 21:25:24 -0800

    Commit

---
+++ Hello/H1P2.py
@@ -0,0 +1,6 @@
+print("Welcome to KiloConverter, please enter the number of miles you wish to convert to kilometers! ")
+miles = int(input("Miles:"))
+conversion_factor = 0.62137119
+kilometers = miles / conversion_factor
+print("is equal to", kilometers, "Kilometers")
+

1e9594372769124dbd1484fdf37f46a8f05c447f
senaloyd <senaloyd@gmail.com>
2019-01-27 20:28:38 -0800

    Progress 1

--- Hello/H1P1.py
+++ Hello/H1P1.py
@@ -1,5 +1,6 @@
-input_string = input("What is your first name? ")
-input_strong = input("What is your last name? ")
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
 age = int(input("What is your age? "))
 confidence = int(input("What is your confidence in programmers between 0-100%? "))
 dog_age = int(age * 7)
+print("Hello", first_name, last_name,", nice to meet you! You might be", age, "in human years, but in dog years you are", dog_age,".")

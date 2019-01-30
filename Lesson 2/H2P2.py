81bf012021b4ce1fae712197de3ff1d83d5b9403
senaloyd <senaloyd@gmail.com>
2019-01-29 20:21:55 -0800

    Final H2P2

--- Lesson 2/H2P2.py
+++ Lesson 2/H2P2.py
@@ -4,4 +4,18 @@ author_name = input("Enter the name of the author: ")
 publish_year = int(input("Enter the publish year of the book: "))
 pages_total = int(input("Enter the total number of pages in the book: "))
 
+book_age = 2019 - publish_year
+
 # 1st if-else statement 
+if (book_age <= 10):
+    print("This book is younger than 10 years old.")
+else:
+    print("The book is at least ten years old.")
+
+# 2nd if-elif-else statement
+if (pages_total < 100):
+    print("This book is a short book.")
+elif (pages_total <= 300 >= 100):
+    print("This book is an average book.")
+else:
+    print("This book is a long book.")

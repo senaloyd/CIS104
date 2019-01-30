2ca69980bed9cc38cb39658797361bdd4b99733c
senaloyd <senaloyd@gmail.com>
2019-01-29 19:11:01 -0800

    H2P1 Part 1

---
+++ Lesson 2/H2P1.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age:"))
+age_ten = age + 10
+print("In ten years you will be", age_ten, "years old.")
+ftemp = float(input("Enter the temperature in Fahrenheit:"))
+ctemp = ((ftemp - 32) * 5/9)
+print("The temperature in degrees Celsius is", ctemp )

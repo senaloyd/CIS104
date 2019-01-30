5b271a97d67e4d20ff4bd28dff562f7f44baef00
senaloyd <senaloyd@gmail.com>
2019-01-29 20:00:47 -0800

    Correct comments

--- Lesson 2/H2P1.py
+++ Lesson 2/H2P1.py
@@ -2,7 +2,7 @@
 age = int(input("Enter your age: "))
 age_ten = age + 10
 print("In ten years you will be", age_ten, "years old.")
-# How do I do this and restrict the float to 2 decimals? I found some info, but it doesnt seems to be working.
+# Success! I was able to get it to round when it prints in Celsius.
 ftemp = float(input("Enter the temperature in Fahrenheit: "))
 ctemp = float((ftemp - 32) * 5/9)
 print("The temperature in degrees Celsius is", "{:.2f}".format(ctemp))
---
+++ Lesson 2/H2P2.py
@@ -0,0 +1,7 @@
+# Homework/Lab 2 Practice 2
+book_title = input("Enter the book title: ")
+author_name = input("Enter the name of the author: ")
+publish_year = int(input("Enter the publish year of the book: "))
+pages_total = int(input("Enter the total number of pages in the book: "))
+
+# 1st if-else statement 

db8ec169aa4b954a231f193ca6c6b18a90e253c8
senaloyd <senaloyd@gmail.com>
2019-01-29 19:22:06 -0800

    With comments

--- Lesson 2/H2P1.py
+++ Lesson 2/H2P1.py
@@ -1,6 +1,8 @@
+# Homework 2 Part 1
 age = int(input("Enter your age:"))
 age_ten = age + 10
 print("In ten years you will be", age_ten, "years old.")
+# How do I do this and restrict the float to 2 decimals? I found some info, but it doesnt seems to be working.
 ftemp = float(input("Enter the temperature in Fahrenheit:"))
 ctemp = float((ftemp - 32) * 5/9)
 print("The temperature in degrees Celsius is", ctemp)
